496c697ff4f6c5b599a433aeaf69112c21e0e496
Helio Livramento <helio.paraiso@gmail.com>
2026-04-15 00:07:50 -0300

    correção no endereço da api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://telegram-bot-agent-6gik.onrender.com:8000",
   timeout: 15000,
 });
 
